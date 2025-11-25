a646cafde4024f2f0771163f711bb39e07259bb8
Gabriel Lima <gabriel58221@gmail.com>
2025-11-25 18:37:18 -0300

    Enhance Home page with search functionality and improved table styling. Added input handling for cryptocurrency search and a button to load more cryptocurrencies. Updated CSS for better responsiveness and visual appeal.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,77 @@
 import { BsSearch } from "react-icons/bs";
 import styles from "./Home.module.css";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useState, type FormEvent } from "react";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSearch(e: FormEvent) {
+    e.preventDefault();
+    if (input === "") {
+        alert("Digite o nome da criptomoeda");
+        return;
+    }
+
+    navigate(`/detail/${input}`);
+  }
+
+  function handleGetMore() {
+    alert("Carregando mais criptomoedas...");
+  }
+
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
-        <input type="text" placeholder="Digite o nome da criptomoeda" />
+      <form className={styles.form} onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Digite o nome da criptomoeda"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button type="submit">
-            <BsSearch size={30} color="#FFF"/>
+          <BsSearch size={30} color="#FFF" />
         </button>
       </form>
 
       <table>
         <thead>
-            <tr>
-                <th scope="col">Moeda</th>
-                <th scope="col">Valor mercado</th>
-                <th scope="col">Preço</th>
-                <th scope="col">Volume</th>
-                <th scope="col">Variacao 24h</th>
-            </tr>
+          <tr>
+            <th scope="col">Moeda</th>
+            <th scope="col">Valor mercado</th>
+            <th scope="col">Preço</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Variacao 24h</th>
+          </tr>
         </thead>
         <tbody id="tbody">
-            <tr className={styles.tr}>
-                <td className={styles.tdLabel} data-label="Moeda">
-                    <Link to="/detail/bitcoin">
-                        <span>Bitcoin</span> | BTC
-                    </Link>
-                </td>
-                <td className={styles.tdLabel} data-label="Valor mercado">1T</td>
-                <td className={styles.tdLabel} data-label="Preço">8.000</td>
-                <td className={styles.tdLabel} data-label="Volume">2B</td>
-                <td className={styles.tdLabel} data-label="Variacao 24h">
-                    <span>1.20%</span>
-                </td>
-            </tr>
+          <tr className={styles.tr}>
+            <td className={styles.tdLabel} data-label="Moeda">
+              <Link to="/detail/bitcoin">
+                <span>Bitcoin</span> | BTC
+              </Link>
+            </td>
+            <td className={styles.tdLabel} data-label="Valor mercado">
+              1T
+            </td>
+            <td className={styles.tdLabel} data-label="Preço">
+              8.000
+            </td>
+            <td className={styles.tdLabel} data-label="Volume">
+              2B
+            </td>
+            <td className={styles.tdLabel} data-label="Variacao 24h">
+              <span>1.20%</span>
+            </td>
+          </tr>
         </tbody>
       </table>
+
+      <button className={styles.buttonMore} onClick={handleGetMore}>
+        Carregar mais criptomoedas
+      </button>
     </main>
   );
 }
